4a252b533fcd0b3eeb50ab1367c0c997f521bc5c
peter8472 <peter8472@users.noreply.github.com>
2020-08-24 12:45:36 -0700

    removed last of irs990 from both init.py and irs990.py

--- csvsqlite3/__init__.py
+++ csvsqlite3/__init__.py
@@ -13,13 +13,10 @@ from pathlib import Path
 from xml.dom import minidom
 
 home = os.getenv("USERPROFILE")
-# dldir = os.path.join(home,"Downloads")
+
 s3url = "https://s3.amazonaws.com/irs-form-990/"
 
-#filename = glob.glob(os.path.join(dldir,"irs charity 990", "index2019"))[0]
 
-#filename = glob.glob(os.path.join(dldir,"irs charity 990", "index_2018.csv"))[0]
-#filename = glob.glob(os.path.join(dldir,"irs charity 990", "index_2018.csv"))[0]
 
 
 
@@ -33,41 +30,6 @@ def first_tagval_ifany(tagname):
             print(line.firstChild.nodeValue)
         # print (desc[0].firstChild.nodeValue)
         # return(desc[0].firstChild.nodeValue)
-def save_xml_to_file(object_id):
-    fname = s3url +   "_public.xml"
-    print(fname)
-    u = urllib.request.urlopen(fname)
-    v = u.read()
-    with open(x['OBJECT_ID'],'wb') as outfile:
-        outfile.write(v)
-def get_matching_orgs(orgname,saveorg=None):
-    'gets matching charities, writes their xml files to disk'
-    search = re.compile(orgname,re.IGNORECASE)
-    # print(os.listdir(filename))
-    f = open(filename,"r",encoding="utf-8-sig")
-    reader = csv.DictReader(f)
-    print("names: {}".format(reader.fieldnames))
-    
-    for x in reader:
-        if search.search(x["TAXPAYER_NAME"]):
-            if saveorg != None:
-                saveorg(x['OBJECT_ID'])
-            print(x["TAXPAYER_NAME"])
-            
-            
-def show_data():
-    """prints the name, description, and website of all the files
-    in the directory that end with a digit
-    """
-
-    files = glob.glob("*[0-9]")
-    for x in files:
-        print("=========================")
-        infile = open(x, encoding="utf-8-sig")
-        dom = minidom.parse(infile)
-        name = first_tagval_ifany("BusinessNameLine1Txt")
-        desc = first_tagval_ifany("ActivityOrMissionDesc")
-        website = first_tagval_ifany("WebsiteAddressTxt")
 
 
 class tablemaker(object):
